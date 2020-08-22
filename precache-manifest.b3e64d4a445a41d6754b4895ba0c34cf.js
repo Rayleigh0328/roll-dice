3f810f7a980fa324f8ace2e60ee7964e39cbc7b6
Jingyun Bian <jingbian@amazon.com>
2020-08-22 14:50:13 -0400

    Updates

---
+++ precache-manifest.b3e64d4a445a41d6754b4895ba0c34cf.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c2eb6c2542138069725930642b2f1be6",
+    "url": "/roll-dice/index.html"
+  },
+  {
+    "revision": "4b2d604af3a841fefdef",
+    "url": "/roll-dice/static/css/main.771b370a.chunk.css"
+  },
+  {
+    "revision": "7eca99ae39fdafe3da06",
+    "url": "/roll-dice/static/js/2.c3a546f4.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/roll-dice/static/js/2.c3a546f4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4b2d604af3a841fefdef",
+    "url": "/roll-dice/static/js/main.79051fce.chunk.js"
+  },
+  {
+    "revision": "6f265c8f69b57583ddcd",
+    "url": "/roll-dice/static/js/runtime-main.57052159.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/roll-dice/static/media/logo.5d5d9eef.svg"
+  }
+]);
